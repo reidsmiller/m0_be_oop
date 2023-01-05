02cccf222c0a407e47bc5400c6b95dcb13300fe3
Reid Miller <reid.s.miller@gmail.com>
2023-01-05 16:19:02 -0700

    Add Vampire and Dragon classes

--- oop.rb
+++ oop.rb
@@ -32,7 +32,12 @@ class Vampire
 
     def initialize (name, pet)
         @name = name
-        @pet = "bat"
+        @pet = 
+            if pet == nil
+                "bat"
+            else
+                pet
+            end
         @thirsty = true
     end
 
@@ -41,6 +46,15 @@ class Vampire
     end
 end
 
+#I've tried to get the attribute to default to bat without needing any input but I can't figure out how to get past initialize needing 2 arguments if the pet argument is empty.
+vamp1 = Vampire.new("Gary", nil)
+p vamp1
+
+vamp1.drink
+p vamp1
+
+vamp2 = Vampire.new("Selene", "Succubus")
+p vamp2
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -49,6 +63,35 @@ end
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+
+    def initialize (name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @times_eaten = 0
+        @is_hungry = true
+    end
+
+    def eat
+        @times_eaten = @times_eaten.next
+
+        if @times_eaten >= 4
+            @is_hungry = false
+        end
+    end
+end
+
+drg1 = Dragon.new("Saphira", "Eragon", "Blue")
+p drg1
+
+drg1.eat
+p drg1
+
+drg1.eat
+drg1.eat
+drg1.eat
+p drg1
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -58,3 +101,4 @@ end
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
